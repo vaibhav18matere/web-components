c9f91108958aaa21e5f3fdf0296b406ae4a09963
Vaibhav Matere <vpm00718897@gmail.com>
2023-01-25 17:41:24 +0530

    feat : timer created

--- count-down-timer/src/App.js
+++ count-down-timer/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Timer from "./Timer";
 
 function App() {
+  const onExpired = () => {
+    console.log("expired the callback function");
+  };
+
   return (
     <div className="App">
-      <h1>Coun Down Timer</h1>
+      <Timer onExpired={onExpired} duration={2 * 60 * 60 * 60 * 1000} />
+      {/* change the time duration from here */}
     </div>
   );
 }
