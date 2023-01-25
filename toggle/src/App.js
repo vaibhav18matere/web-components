0dcb9e1ede491a4fa402d19f528e0a0199a66440
Vaibhav Matere <vpm00718897@gmail.com>
2023-01-25 19:13:55 +0530

    set requirements

--- toggle/src/App.js
+++ toggle/src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import React from "react";
 
-function App() {
+function App(props) {
+  const { coworkers } = props;
+  console.log(coworkers);
+
   return (
     <div className="App">
       <h1>Status Toggler</h1>
     </div>
   );
 }
+
+App.defaultProps = {
+  coworkers: [
+    { first_name: "Vaibhav", last_name: "Matere" },
+    { first_name: "Shubham", last_name: "Matere" },
+    { first_name: "Deepak", last_name: "Matere" },
+  ],
+};
 
 export default App;
